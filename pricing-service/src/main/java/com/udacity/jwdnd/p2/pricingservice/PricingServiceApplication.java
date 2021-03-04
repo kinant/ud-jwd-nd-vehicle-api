c4902938a8909518ed1ac922e13ba37cf99ded11
KinanT <kinan.t@gmail.com>
2021-03-04 10:40:55 -0600

    Added Eureka Server

--- pricing-service/src/main/java/com/udacity/jwdnd/p2/pricingservice/PricingServiceApplication.java
+++ pricing-service/src/main/java/com/udacity/jwdnd/p2/pricingservice/PricingServiceApplication.java
@@ -1,13 +1,26 @@
 package com.udacity.jwdnd.p2.pricingservice;
 
+import com.udacity.jwdnd.p2.pricingservice.domain.price.PriceRepository;
+import com.udacity.jwdnd.p2.pricingservice.service.PricingService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class PricingServiceApplication {
+public class PricingServiceApplication implements CommandLineRunner {
+
+    @Autowired
+    private PriceRepository priceRepository;
 
     public static void main(String[] args) {
         SpringApplication.run(PricingServiceApplication.class, args);
     }
 
+    @Override
+    public void run(String... args) throws Exception {
+        for(int i = 1; i < 20; i++){
+            priceRepository.save( PricingService.getPrice((long) i));
+        }
+    }
 }
