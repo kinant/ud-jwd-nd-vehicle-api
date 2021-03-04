1df26661c5e2051a602b78a81b5143a866fb09aa
KinanT <kinan.t@gmail.com>
2021-03-04 10:52:58 -0600

    Created Pricing Service Client
    Works. Registered to Eureka Server.

--- pricing-service/src/main/java/com/udacity/jwdnd/p2/pricingservice/domain/price/PriceRepository.java
+++ pricing-service/src/main/java/com/udacity/jwdnd/p2/pricingservice/domain/price/PriceRepository.java
@@ -1,8 +1,8 @@
 package com.udacity.jwdnd.p2.pricingservice.domain.price;
 
-import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface PriceRepository extends JpaRepository<Price, Long> {
+public interface PriceRepository extends CrudRepository<Price, Long> {
 }
